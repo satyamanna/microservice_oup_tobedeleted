d6f553805ab7f4f603484233a5c2490afa7ef673
718321 <718321@PCIN469148.cts.com>
2019-12-31 18:50:54 +0530

    done for  implementing zuul

--- Gateway/src/main/java/com/cts/oup/GateWay.java
+++ Gateway/src/main/java/com/cts/oup/GateWay.java
@@ -3,9 +3,10 @@ package com.cts.oup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableEurekaClient
-@Enablezu
+@EnableZuulProxy
 @SpringBootApplication
 public class GateWay {
 
